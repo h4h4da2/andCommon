4674b269852edca37dbd359d513169a131e93438
fork <h4h4da@163.com>
2021-07-29 14:46:51 +0800

    CHG:添加支付宝支付

--- pay/src/main/java/com/hhda/pay/PayManager.kt
+++ pay/src/main/java/com/hhda/pay/PayManager.kt
@@ -1,9 +1,43 @@
 package com.hhda.pay
 
+import android.app.Activity
+import android.os.Handler
+import android.os.Looper
+import com.alipay.sdk.app.PayTask
+import com.hhda.pay.alipay.PayResult
+import com.hhda.pay.constants.PayCode
+import com.hhda.pay.constants.PayMethod
+import java.util.concurrent.atomic.AtomicBoolean
+
 class PayManager {
 
-    fun aliPay() {
+    //是否正在支付的标志位
+    private val isPaying: AtomicBoolean = AtomicBoolean(false)
 
+    fun aliPay(orderInfo: String, callback: PayCallback?, activity: Activity) {
+
+        if (isPaying.get()) return
+        isPaying.set(true)
+
+        val payRunnable = Runnable {
+            val alipay = PayTask(activity)
+            val rowResult = alipay.payV2(orderInfo, true)
+
+            val payResult = PayResult(rowResult)
+
+            val payCode = when (payResult.resultStatus) {
+                "9000" -> PayCode.USER_PAY_SUCCEED
+                "6001" -> PayCode.USER_PAY_CANCEL
+                else -> PayCode.PAY_FAIL
+            }
+            Handler(Looper.getMainLooper()).post {
+                callback?.onPay(PayMethod.ALI_PAY, payCode, payResult)
+            }
+            isPaying.set(false)
+        }
+
+        val payThread = Thread(payRunnable)
+        payThread.start()
     }
 
     fun wechatPay() {
